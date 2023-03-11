325bd2ab822790ad7b50f335a0c55817e35987dc
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-03-11 15:52:16 -0800

    unwanted config

--- src/render/order.rs
+++ src/render/order.rs
@@ -1,10 +1,9 @@
 use super::node::Node;
 use clap::ValueEnum;
-use serde::Deserialize;
 use std::{cmp::Ordering, convert::From};
 
 /// Order in which to print nodes.
-#[derive(Copy, Clone, Debug, Deserialize, ValueEnum, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, ValueEnum, PartialEq, Eq, PartialOrd, Ord)]
 pub enum SortType {
     /// Sort entries by file name
     Name,
