aeaf10a126425a1defb244fbf86eb70527e58965
Andreas Linz <klingt.net@gmail.com>
2023-03-22 17:03:22 +0100

    fixup! Add unit test

--- tests/dirs_only.rs
+++ tests/dirs_only.rs
@@ -5,19 +5,13 @@ mod utils;
 #[test]
 fn level() {
     assert_eq!(
-        utils::run_cmd(&[
-            "--sort",
-            "name",
-            "--no-config",
-            "--dirs-only",
-            "tests/data"
-        ]),
+        utils::run_cmd(&["--sort", "name", "--no-config", "--dirs-only", "tests/data"]),
         indoc!(
             "
-            data 
-            ├─ dream_cycle 
-            ├─ lipsum 
-            └─ the_yellow_king"
+            data (1.21 KiB)
+            ├─ dream_cycle (308 B)
+            ├─ lipsum (446 B)
+            └─ the_yellow_king (143 B)"
         ),
         "Failed to print dirs-only."
     )
