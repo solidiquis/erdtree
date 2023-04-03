ac9026ff0730637022ddf55931a174d97ab7bc0d
Jan Scheer <jhscheer@users.noreply.github.com>
2023-04-03 17:19:08 +0200

    add test for glob patterns from stdin

--- tests/glob.rs
+++ tests/glob.rs
@@ -80,3 +80,58 @@ fn iglob() {
         )
     )
 }
+
+#[test]
+fn glob_stdin() {
+    use std::io::Write;
+    use std::process::{Command, Stdio};
+    use strip_ansi_escapes::strip as strip_ansi_escapes;
+
+    let cmd = Command::new("cargo")
+        .args([
+            "run",
+            "--",
+            "--threads",
+            "1",
+            "--no-config",
+            "--sort",
+            "name",
+            "-",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let stdin = String::from("cassildas_song.md\nnemesis.txt\n");
+    write!(cmd.stdin.as_ref().unwrap(), "{}", stdin).unwrap();
+    let output = cmd.wait_with_output().unwrap();
+
+    assert_eq!(
+        String::from_utf8(strip_ansi_escapes(output.stdout).unwrap()).unwrap(),
+        indoc!(
+            "
+            erdtree (304 B)
+            ├─ assets 
+            ├─ src 
+            │  ├─ fs 
+            │  └─ render 
+            │     ├─ context 
+            │     ├─ disk_usage 
+            │     └─ tree 
+            │        └─ node 
+            └─ tests (304 B)
+               ├─ data (304 B)
+               │  ├─ dream_cycle 
+               │  ├─ lipsum 
+               │  ├─ nemesis.txt (161 B)
+               │  └─ the_yellow_king (143 B)
+               │     └─ cassildas_song.md (143 B)
+               └─ utils 
+
+            "
+        )
+    );
+    assert!(output.status.success());
+}
