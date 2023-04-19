422bb8b630883872ef52ec87c520589af7fec408
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-04-19 15:15:04 -0700

    reduce redundant code

--- src/fs/xattr.rs
+++ src/fs/xattr.rs
@@ -20,7 +20,6 @@ pub trait ExtendedAttr {
 }
 
 /// Checks to see if a directory entry referred to by `path` has extended attributes.
-#[cfg(target_os = "macos")]
 unsafe fn has_xattrs(path: &Path) -> bool {
     use libc::{c_char, listxattr};
 
@@ -30,18 +29,9 @@ unsafe fn has_xattrs(path: &Path) -> bool {
         slice_ptr.cast::<c_char>()
     };
 
-    0 < listxattr(path_ptr, ptr::null_mut::<c_char>(), 0, 0)
-}
+    #[cfg(target_os = "linux")]
+    return 0 < listxattr(path_ptr, ptr::null_mut::<c_char>(), 0);
 
-#[cfg(target_os = "linux")]
-unsafe fn has_xattrs(path: &Path) -> bool {
-    use libc::{c_char, listxattr};
-
-    let path_ptr = {
-        let slice = path.as_os_str().as_bytes();
-        let slice_ptr = slice.as_ptr();
-        slice_ptr.cast::<*const c_char>()
-    };
-
-    0 < listxattr(path_ptr, ptr::null_mut::<c_char>(), 0)
+    #[cfg(target_os = "macos")]
+    return 0 < listxattr(path_ptr, ptr::null_mut::<c_char>(), 0, 0);
 }
