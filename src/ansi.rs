5b8f6777988f3e424b52ce5f4b95a29a09e72c99
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-04-22 22:22:53 -0700

    doc comment

--- src/ansi.rs
+++ src/ansi.rs
@@ -1,6 +1,5 @@
-/// Trait that allows ANSI escaped strings to be truncated in a manner that preserves the ANSI
-/// style/color sequences, meaning only the `char`s that aren't part of the ANSI escapes are
-/// considered when discussing length. Consider the following:
+/// Trait that provides functionality to ANSI escaped strings to be truncated in a manner that
+/// preserves the ANSI color/style escape sequences. Consider the following:
 ///
 /// ```
 /// // "\u{1b}[1;31mHello World\u{1b}[0m"
@@ -9,27 +8,25 @@
 ///
 /// Truncating the above to a length of 5 would result in:
 ///
-/// ```
-/// // "\u{1b}[1;31mHello\u{1b}[0m"
-/// ```
+/// `"\u{1b}[1;31mHello\u{1b}[0m"`
 ///
 /// NOTE: This is being used for a very particular use-case and isn't comprehensive enough to
-/// handle all types of ANSI escaped sequences, only color/style related ones.
+/// handle all types of ANSI escaped sequences, only color/style related ones. It also makes some
+/// assumptions that are valid only for this program, namely that all relevant grapheme clusters
+/// are at most sized to a single `char`, so truncating to any arbitrary length will always result
+/// in a coherent output.
 #[allow(clippy::module_name_repetitions)]
 pub trait AnsiEscaped: AsRef<str> {
     fn truncate(&self, new_len: usize) -> String {
         let mut open_sequence = false;
-
         let mut resultant = String::new();
-
         let mut char_count = 0;
-
         let mut chars = self.as_ref().chars();
 
         'outer: while let Some(ch) = chars.next() {
-            if ch == '\u{1b}' && !open_sequence {
-                resultant.push(ch);
+            resultant.push(ch);
 
+            if ch == '\u{1b}' && !open_sequence {
                 for code in chars.by_ref() {
                     resultant.push(code);
 
@@ -38,11 +35,7 @@ pub trait AnsiEscaped: AsRef<str> {
                         continue 'outer;
                     }
                 }
-            }
-
-            if ch == '\u{1b}' && open_sequence {
-                resultant.push(ch);
-
+            } else if ch == '\u{1b}' && open_sequence {
                 for code in chars.by_ref() {
                     resultant.push(code);
 
@@ -52,8 +45,6 @@ pub trait AnsiEscaped: AsRef<str> {
                     }
                 }
             }
-
-            resultant.push(ch);
             char_count += 1;
 
             if char_count == new_len {
