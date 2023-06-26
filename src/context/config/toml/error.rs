76c2973c2f544e3d9b826e9a77c218013b42a6b5
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2023-06-25 18:38:47 -0700

    update error message

--- src/context/config/toml/error.rs
+++ src/context/config/toml/error.rs
@@ -8,7 +8,7 @@ pub enum Error {
     #[error("The configuration file is improperly formatted")]
     InvalidFormat(#[from] ConfigError),
 
-    #[error("Alternate configuration '{0}' was not found in '.erdtree.toml'")]
+    #[error("Named table '{0}' was not found in '.erdtree.toml'")]
     MissingAltConfig(String),
 
     #[error("'#{0}' is required to be a pointer-sized unsigned integer type")]
